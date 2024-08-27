18ffb0c201a89849b5d2f107eb8d355fe3533604
TiagoAlves <tiago2018cel123@gmail.com>
2024-08-27 09:28:50 -0300

    add files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const btnDec = document.querySelector('button');
+const result = document.querySelector('#result');
+
+function inverteTexto(str) {
+    // const regex = /^[a-zA-Z\s]+$/
+    // const cleandedStr = str.replace(regex, '').toLowerCase();
+    const letras = str.split(' ');
+    const reversedWords = letras.map(word =>
+    word.toLowerCase().split('').reverse().join(''));
+
+    return reversedWords.join(' ');
+};
+
+btnDec.addEventListener('click', () => {
+    const inputText = document.querySelector('#input-text');
+    const result = inverteTexto(inputText.value);
+    document.querySelector('#result').innerText = `Maior letra é: ${result}`
+})
